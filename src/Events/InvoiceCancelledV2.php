f76c8d744d1fce4437e3a3cbe2ec0bc3c31fb210
Wiebe Kloosterman <wiebe@kloosterman.eu>
2023-10-20 13:49:40 -0400

    Added Invoice Events

---
+++ src/Events/InvoiceCancelledV2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ProtoneMedia\LaravelPaddle\Events;
+
+/**
+ * Fired when a invoice is cancelled.
+ */
+class InvoiceCancelledV2 extends Event
+{
+
+}
---
+++ src/Events/InvoiceOverdueV2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ProtoneMedia\LaravelPaddle\Events;
+
+/**
+ * Fired when a invoice is overdue.
+ */
+class InvoiceOverdueV2 extends Event
+{
+
+}
